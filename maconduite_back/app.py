a19baf7ebd9c298c000acf285d9b5327ad452b50
Michel SADEU <michelsadeungakou@gmail.com>
2024-04-01 12:48:21 +0200

    change sqlite database url to postgres database url

--- maconduite_back/app.py
+++ maconduite_back/app.py
@@ -10,6 +10,7 @@ db = flask_sqlalchemy.SQLAlchemy()
 
 from maconduite_back.routes.auth import auth
 
+
 def create_app():
     """
     This is the main function used to generate a Flask object with configuration parameters.
@@ -22,36 +23,38 @@ def create_app():
         See also https://flask.palletsprojects.com/en/2.0.x/api/#flask.Flask
     """
 
-    app = Flask(__name__, static_folder='../maonduite_front/web-build', static_url_path='/')
+    app = Flask(
+        __name__, static_folder="../maconduite_frontend/build", static_url_path="/"
+    )
     app.config.update(
-        SQLALCHEMY_DATABASE_URI = (
-            'sqlite:///'
-            + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'debug.db')
+        SQLALCHEMY_DATABASE_URI=(
+            # 'sqlite:///'
+            # + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'debug.db')
+            "postgresql://MICHEL:MICHELPOSTGRES@db:5432/MACONDUITE"
         ),
-        SQLALCHEMY_TRACK_MODIFICATIONS = False,
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
         # SECRET_KEY = JWT_SECRET,
-        JWT_SECRET = '\xe1\xd8\x5f\x3a\x53\x05\x7b\x66\x6d\xf8\x20\x9b\xb0\xb3\xa5\x4a',
-        JWT_ALGORITHM = 'HS256',
-        JWT_EXP_DELTA_HOURS = 2,
-        #UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)),'uploaded_files'),
+        JWT_SECRET="\xe1\xd8\x5f\x3a\x53\x05\x7b\x66\x6d\xf8\x20\x9b\xb0\xb3\xa5\x4a",
+        JWT_ALGORITHM="HS256",
+        JWT_EXP_DELTA_HOURS=2,
+        # UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)),'uploaded_files'),
         # ALLOWED_EXTENSIONS = {'DAT'},
-        MAX_CONTENT_LENGTH = 16 * 1024 * 1024,          # = 16 MB
-        SQLALCHEMY_ECHO = False,
+        MAX_CONTENT_LENGTH=16 * 1024 * 1024,  # = 16 MB
+        SQLALCHEMY_ECHO=False,
     )
 
     # initializing the flask_sqlalchemy object with the application context
     db.init_app(app)
     add_mock_data(app)
 
-
     app.register_blueprint(auth)
-
 
     @app.route("/")
     def hello_world():
         return "<p>Hello, World!</p>"
-    
+
     return app
+
 
 def add_mock_data(app):
     """
@@ -66,7 +69,7 @@ def add_mock_data(app):
         from datetime import datetime
 
         # logger.info('Initializing database with testing data.')
-        logger.info('> Creating new tables.')
+        logger.info("> Creating new tables.")
         db.create_all()
         db.session.commit()
 
@@ -77,4 +80,4 @@ def add_mock_data(app):
         # db.session.commit()
         # print(user1.json_out())
         # print(user2.json_out())
-        logger.info('Database has been successfully connected.')
+        logger.info("Database has been successfully connected.")
